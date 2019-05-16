16c0f1ad1431d4e97aec1f102aa6877b1ce0b461
Tim 'mithro' Ansell <me@mith.ro>
2019-05-15 21:33:34 -0700

    Require docutils.
    Fixes;
    ```
    ERROR: Complete output from command python setup.py egg_info:
    ERROR: Traceback (most recent call last):
      File "<string>", line 1, in <module>
      File "/tmp/pip-install-vcb4byda/sphinxcontrib-domaintools/setup.py", line 11, in <module>
        import domaintools
      File "sphinxcontrib/domaintools.py", line 25, in <module>
        from docutils import nodes
    ModuleNotFoundError: No module named 'docutils'
    ```

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ This package contains tools for easy sphinx domain creation.
 sys.path.insert(0, 'sphinxcontrib')
 import domaintools
 
-requires = ['Sphinx>=1.0']
+requires = ['Sphinx>=1.0', 'docutils>=0.14']
 
 setup(
     name='sphinxcontrib-domaintools',
